6472571a6299e4a777457092aa21d94ff27a8155
Jean <jeanlucafp@gmail.com>
2021-08-06 10:25:32 -0300

    google auth

---
+++ src/service/fire.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHYgBhNSF9ceEpMLbD-Zgyqn33gSGTQwQ",
+  authDomain: "binno-software.firebaseapp.com",
+  projectId: "binno-software",
+  storageBucket: "binno-software.appspot.com",
+  messagingSenderId: "816935504379",
+  appId: "1:816935504379:web:3206e97a57f8c05aeaa35e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
