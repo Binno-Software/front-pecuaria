d5d8b11b800a13ba03dc55bf2a2121d5aadd9d0e
Jeanluca <jeanlucafp@gmail.com>
2021-08-07 09:29:15 -0300

    Update fire.js

--- src/service/fire.js
+++ src/service/fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHYgBhNSF9ceEpMLbD-Zgyqn33gSGTQwQ",
-  authDomain: "binno-software.firebaseapp.com",
-  projectId: "binno-software",
-  storageBucket: "binno-software.appspot.com",
-  messagingSenderId: "816935504379",
-  appId: "1:816935504379:web:3206e97a57f8c05aeaa35e"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.APP_ID,
+  projectId: process.env.AUTH_DOMAIN,
+  storageBucket: process.env.BUCKET,
+  messagingSenderId: process.env.MESSAGE_ID,
+  appId: process.env.PROJECT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
