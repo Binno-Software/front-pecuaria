eda6c4a9f1de1c611874931b6036e7c672998220
Jeanluca <jeanlucafp@gmail.com>
2022-07-03 17:57:44 -0300

    dado start na feature

---
+++ src/pages/comercializacao/ProcessoVacinacao/index.js
@@ -0,0 +1,297 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  Box,
+  Container,
+  makeStyles,
+  TextField,
+  Card,
+  Grid,
+  Paper,
+  Button,
+  Tab,
+  Tabs
+} from '@material-ui/core';
+import Page from 'src/components/Page';
+import api from 'src/service/api';
+import EmptyData from 'src/components/EmptyData';
+import GridAnimaisSelecionados from './GridAnimaisSelecionados';
+import GridAnimais from './GridAnimais';
+import FazendaSelect from 'src/components/FazendaSelect';
+import MedicamentoSelect from 'src/components/MedicamentoSelect';
+import { toastSuccess } from 'src/utils/toast';
+import { useNavigate } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3),
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+const ProcessoVacinacao = () => {
+  const classes = useStyles();
+  const [values, setValues] = useState({});
+  const [data, setData] = useState({
+    content: []
+  });
+  const [loading, setLoading] = useState(true);
+  const [limit, setLimit] = useState(5);
+  const [page, setPage] = useState(0);
+  const [fazenda, setFazenda] = useState({});
+  const [medicamento, setMedicamento] = useState({});
+  const [tab, setTab] = useState(0);
+  const [animaisSelecionados, setAnimaisSelecionados] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    api.get('animais', {
+      params: {
+        size: limit,
+        page,
+      }
+    }).then((response) => {
+      setData(response.data);
+      setLoading(false);
+    });
+  }, [limit, page]);
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const reload = useCallback((_limit, offset) => {
+    setLoading(true);
+    setLimit(_limit);
+    setPage(offset);
+  }, []);
+
+  const addAnimal = useCallback((data) => {
+    if (data instanceof Array) {
+      setAnimaisSelecionados(data);
+      return;
+    }
+
+    setAnimaisSelecionados([data, ...animaisSelecionados]);
+  }, [animaisSelecionados]);
+
+  const addFazenda = useCallback(
+    (_fazenda) => {
+      setFazenda({
+        id: _fazenda
+      });
+    },
+    [setFazenda]
+  );
+
+  const addMedicamento = useCallback(
+    (_medicamento) => {
+      setMedicamento({
+        id: _medicamento
+      });
+    },
+    [setMedicamento]
+  );
+
+  const iniciarProcesso = useCallback(() => {
+    api.post('vacinacao', {
+      medicamentoId: medicamento.id,
+      animaisId: animaisSelecionados.map(animal => animal.id)
+    }).then(() => {
+      toastSuccess('Processo de vacinação criado com sucesso');
+      navigate('../../animais');
+    });
+  }, [medicamento, animaisSelecionados, navigate]);
+
+  const filtrarPesquisa = useCallback(() => {
+    setLoading(true);
+    const filter = {
+      numero: values.numeroEspecifico || null,
+      numeroInicial: values.numeroInicial || null,
+      numeroFinal: values.numeroFinal || null,
+      size: limit,
+      page,
+    };
+
+    api.get('animais', { params: filter }).then((response) => {
+      setData(response.data);
+      setLoading(false);
+    });
+  }, [values, limit, page]);
+
+  return (
+    <Page
+      className={classes.root}
+      title="Vacinação"
+    >
+      <Container maxWidth={false}>
+
+        <Card>
+
+          <div className={classes.root}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <Paper className={classes.paper}>
+                  <FazendaSelect fazendaSelected={fazenda.id} addFazenda={addFazenda} />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Paper className={classes.paper}>
+                  <TextField
+                    fullWidth
+                    label="Estado do animal"
+                    name="selecionado"
+                    onChange={handleChange}
+                    select
+                    SelectProps={{ native: true }}
+                    value={values.selecionado}
+                    variant="outlined"
+                  />
+                </Paper>
+              </Grid>
+              <Grid item xs={6} sm={3}>
+                <Paper className={classes.paper}>
+                  <TextField
+                    label="Numero inicial"
+                    margin="normal"
+                    name="numeroInicial"
+                    onChange={handleChange}
+                    type="text"
+                    value={values.numeroInicial}
+                    variant="outlined"
+                  />
+                </Paper>
+              </Grid>
+              <Grid item xs={6} sm={3}>
+                <Paper className={classes.paper}>
+                  <TextField
+                    label="Numero final"
+                    margin="normal"
+                    name="numeroFinal"
+                    onChange={handleChange}
+                    type="text"
+                    value={values.numeroFinal}
+                    variant="outlined"
+                  />
+                </Paper>
+              </Grid>
+              <Grid item xs={6} sm={3}>
+                <Paper className={classes.paper}>
+                  <TextField
+                    label="Numero específico"
+                    margin="normal"
+                    name="numeroEspecifico"
+                    onChange={handleChange}
+                    type="text"
+                    value={values.numeroEspecifico}
+                    variant="outlined"
+                  />
+                </Paper>
+              </Grid>
+              <Grid item xs={6} sm={3}>
+                <Paper className={classes.paper}>
+                  <TextField
+                    label="Idade em dias"
+                    margin="normal"
+                    name="idadeEmDias"
+                    onChange={handleChange}
+                    type="text"
+                    value={values.idadeEmDias}
+                    variant="outlined"
+                  />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Paper className={classes.paper}>
+                  <MedicamentoSelect medicamentoSelected={medicamento.id} addMedicamento={addMedicamento} />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Paper className={classes.paper}>
+                  <Button
+                    fullWidth
+                    color="primary"
+                    variant="contained"
+                    onClick={iniciarProcesso}
+                  >
+                    Medicar
+                  </Button>
+                </Paper>
+                <Paper className={classes.paper}>
+                  <Button
+                    fullWidth
+                    color="primary"
+                    variant="contained"
+                    onClick={filtrarPesquisa}
+                  >
+                    Aplicar filtros
+                  </Button>
+                </Paper>
+              </Grid>
+            </Grid>
+          </div>
+        </Card>
+
+        {data.content.length > 0 ? (
+          <>
+            <Tabs value={tab} onChange={(_, newValue) => setTab(newValue)}>
+              <Tab label="animais" {...a11yProps(0)} />
+              <Tab label="animais selecionados" {...a11yProps(1)} />
+            </Tabs>
+            <TabPanel value={tab} index={0}>
+              <Box mt={3}>
+                <GridAnimais loading={loading} addAnimal={addAnimal} data={data} reload={reload} page={page} limit={limit} />
+              </Box>
+            </TabPanel>
+            <TabPanel value={tab} index={1}>
+              <Box mt={3}>
+                <GridAnimaisSelecionados data={animaisSelecionados} />
+              </Box>
+            </TabPanel>
+          </>
+        ) : (
+          <EmptyData />
+        )}
+      </Container>
+    </Page>
+  );
+};
+
+export default ProcessoVacinacao;
---
+++ src/pages/comercializacao/Results.js
@@ -0,0 +1,229 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import {
+  Box,
+  Card,
+  Checkbox,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+  makeStyles,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button
+} from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  avatar: {
+    marginRight: theme.spacing(2)
+  }
+}));
+
+const Results = ({
+  className, data, reload, page, limit, ...rest
+}) => {
+  const classes = useStyles();
+  const funcionarios = data.content;
+  const [selectedIds, setSelectedIds] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(undefined);
+
+  // eslint-disable-next-line
+  const handleClickOpen = (registro) => {
+    setOpen(true);
+    setSelectedItem(registro);
+    console.log(registro);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSelectAll = (event) => {
+    let newSelectedIds;
+
+    if (event.target.checked) {
+      newSelectedIds = funcionarios.map((element) => element.id);
+    } else {
+      newSelectedIds = [];
+    }
+
+    setSelectedIds(newSelectedIds);
+  };
+
+  const handleSelectOne = (event, id) => {
+    const selectedIndex = selectedIds.indexOf(id);
+    let newSelectedIds = [];
+
+    if (selectedIndex === -1) {
+      newSelectedIds = newSelectedIds.concat(selectedIds, id);
+    } else if (selectedIndex === 0) {
+      newSelectedIds = newSelectedIds.concat(selectedIds.slice(1));
+    } else if (selectedIndex === selectedIds.length - 1) {
+      newSelectedIds = newSelectedIds.concat(selectedIds.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelectedIds = newSelectedIds.concat(
+        selectedIds.slice(0, selectedIndex),
+        selectedIds.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelectedIds(newSelectedIds);
+  };
+
+  const handleLimitChange = (event) => {
+    reload(event.target.value, page);
+  };
+
+  const handlePageChange = (_, newPage) => {
+    reload(limit, newPage);
+  };
+
+  return (
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <PerfectScrollbar>
+        <Box minWidth={1050}>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">Você tem certeza que quer reverter esse Processo?</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {selectedItem?.id}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Voltar
+              </Button>
+              <Button color="primary" autoFocus>
+                Excluir
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    checked={selectedIds.length === funcionarios.length}
+                    color="primary"
+                    indeterminate={
+                      selectedIds.length > 0
+                      && selectedIds.length < funcionarios.length
+                    }
+                    onChange={handleSelectAll}
+                  />
+                </TableCell>
+                <TableCell>
+                  Data
+                </TableCell>
+                <TableCell>
+                  Total animais
+                </TableCell>
+                <TableCell>
+                  Medicamento
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {funcionarios.slice(0, limit).map((element) => (
+                <TableRow
+                  hover
+                  key={element.id}
+                  selected={selectedIds.indexOf(element.id) !== -1}
+                >
+                  <TableCell padding="checkbox">
+                    <Checkbox
+                      checked={selectedIds.indexOf(element.id) !== -1}
+                      onChange={(event) => handleSelectOne(event, element.id)}
+                      value="true"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Box
+                      alignItems="center"
+                      display="flex"
+                    >
+                      <Typography
+                        color="textPrimary"
+                        variant="body1"
+                      >
+                        {element.dataProcesso}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                  <TableCell>
+                    <Box
+                      alignItems="center"
+                      display="flex"
+                    >
+                      <Typography
+                        color="textPrimary"
+                        variant="body1"
+                      >
+                        {element.totalAnimaisVacinados}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                  <TableCell>
+                    <Box
+                      alignItems="center"
+                      display="flex"
+                    >
+                      <Typography
+                        color="textPrimary"
+                        variant="body1"
+                      >
+                        {element.medicamento.descricao}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                  {/* <TableCell padding="checkbox">
+                    <CreateIcon onClick={() => update(element)} />
+                  </TableCell> */}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </PerfectScrollbar>
+      <TablePagination
+        component="div"
+        count={data.totalElements}
+        onChangePage={handlePageChange}
+        onChangeRowsPerPage={handleLimitChange}
+        page={page}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[10, 25, 50]}
+        labelRowsPerPage="Registros por pagina"
+      />
+    </Card>
+  );
+};
+
+Results.propTypes = {
+  className: PropTypes.string,
+  data: PropTypes.object,
+  reload: PropTypes.func,
+  page: PropTypes.number,
+  limit: PropTypes.number,
+};
+
+export default Results;
